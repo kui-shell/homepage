ab81327a04b044ffc8ecaf5a60fea8aa777645be
Yeray Darias <ydarias@gmail.com>
2019-05-14 18:43:24 +0200

    Fixed cluster name

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ docker push us.icr.io/kui-shell/kui-landing:$TRAVIS_BUILD_NUMBER
 
 # Deploy to K8s cluster
 
-STORE_KUBECONFIG=$(ibmcloud ks cluster-config iqs-eve | tail -n 1)
+STORE_KUBECONFIG=$(ibmcloud ks cluster-config iqs-events | tail -n 1)
 eval $STORE_KUBECONFIG
 
 helm upgrade -i kui-landing chart
