91b0e0ce7a57f9dc2b324c54b6781195d6276a5a
Yeray Darias <ydarias@gmail.com>
2019-05-14 18:38:54 +0200

    Selected K8s cluster during deployment

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,9 @@ docker build -t kui-landing .
 docker tag kui-landing us.icr.io/kui-shell/kui-landing:$TRAVIS_BUILD_NUMBER
 docker push us.icr.io/kui-shell/kui-landing:$TRAVIS_BUILD_NUMBER
 
+# Deploy to K8s cluster
+
+STORE_KUBECONFIG=$(ibmcloud ks cluster-config iqs-eve | tail -n 1)
+eval $STORE_KUBECONFIG
+
 helm upgrade -i kui-landing chart
