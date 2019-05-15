58f7e0f585ceda24e0b4e59e29756f8f456361b2
Yeray Darias <ydarias@gmail.com>
2019-05-15 13:02:43 +0200

    Added Countly tracking

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react");
+const { Analytics } = require("./src/components/analytics");
+
+exports.wrapRootElement = ({ element }) => {
+    return (
+        <>
+            {element}
+            <noscript><img alt="analytics" src='https://analytics.iqs-events.us-south.containers.appdomain.cloud/pixel.png?app_key=3cb27b4529db2bf59eeeecd05a317135a640f1b4&begin_session=1'/></noscript>
+            <Analytics/>
+        </>
+    );
+}
