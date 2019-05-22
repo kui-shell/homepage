da66f5abb35e7e7a0ce9e361f68c93f18ea6c889
Yeray Darias <ydarias@gmail.com>
2019-05-22 09:48:30 +0200

    Resetted Gatsby’s CSS

--- src/components/header.js
+++ src/components/header.js
@@ -3,54 +3,19 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header>
+    <div>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link to="/">
           {siteTitle}
         </Link>
-        <Link
-          to="/page-3"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link to="/page-3">
           Download
         </Link>
-        <Link
-          to="/page-3"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link to="/page-3">
           GitHub
         </Link>
-        <Link
-          to="/docs"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+        <Link to="/docs">
           Documentation
         </Link>
       </h1>
@@ -63,7 +28,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `Kui`,
 }
 
 export default Header
