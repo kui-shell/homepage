a79ba84d9009fdf618ee88746dee1f8f8aebf677
Abdón Rodríguez Davila <a@abdonrd.com>
2019-06-11 12:36:02 +0200

    Use static files

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ExternalScripts from "../components/external-scripts"
 
-import copyIcon from "../ui/copy.svg"
-import downloadIcon from "../ui/download.svg"
-import pagelinkIcon from "../ui/pagelink.svg"
-
-import video1 from "../videos/video1.mp4"
-import video2 from "../videos/video2.mp4"
-import video3 from "../videos/video3.mp4"
-import video4 from "../videos/video4.mp4"
-
 import "./index.css"
 
 const IndexPage = () => (
@@ -45,7 +36,7 @@ const IndexPage = () => (
         <div class="ibm-grid-col-lg-16-1"></div>
         <div class="ibm-grid-col-lg-16-14">
           <video muted autoplay controls loop class="kui-hero-video">
-          <source src={video1} type="video/mp4" />
+          <source src={'videos/video1.mp4'} type="video/mp4" />
           Your browser does not support the video tag.
           </video>
         </div>
@@ -58,14 +49,14 @@ const IndexPage = () => (
             <h3 class="ibm-type-a">Brew</h3>
             <button class="kui-codecopy">
               <span class="ibm-type-b ibm-type-mono kui-codecopy-content" data-cmd="cmd1">cmd1</span>
-              <img src={copyIcon} alt="Copy" />
+              <img src={'ui/copy.svg'} alt="Copy" />
             </button>
           </div>
           <div class="kui-codepaste">
             <h3 class="ibm-type-a">NPM</h3>
             <button class="kui-codecopy">
               <span class="ibm-type-b ibm-type-mono kui-codecopy-content" data-cmd="cmd2">cmd2</span>
-              <img src={copyIcon} alt="Copy" />
+              <img src={'ui/copy.svg'} alt="Copy" />
             </button>
           </div>
         </ul>
@@ -73,31 +64,31 @@ const IndexPage = () => (
           <h3 class="ibm-type-a">Resources</h3>
           <a href="#" class="kui-resources-link kui-download-link hidden" data-os="mac">
             <span>Download for macOS</span>
-            <img src={downloadIcon} alt="Download" />
+            <img src={'ui/download.svg'} alt="Download" />
           </a>
           <a href="#" class="kui-resources-link kui-download-link hidden" data-os="win">
             <span>Download for Windows</span>
-            <img src={downloadIcon} alt="Download" />
+            <img src={'ui/download.svg'} alt="Download" />
           </a>
           <a href="#" class="kui-resources-link kui-download-link hidden" data-os="lindeb">
             <span>Download for Linux .deb</span>
-            <img src={downloadIcon} alt="Download" />
+            <img src={'ui/download.svg'} alt="Download" />
           </a>
           <a href="#" class="kui-resources-link kui-download-link hidden" data-os="linrpm">
             <span>Download for Linux .rpm</span>
-            <img src={downloadIcon} alt="Download" />
+            <img src={'ui/download.svg'} alt="Download" />
           </a>
           <a href="#" class="kui-resources-link">
             <span>All Downloads</span>
-            <img src={pagelinkIcon} alt="Link" />
+            <img src={'ui/pagelink.svg'} alt="Link" />
           </a>
           <a href="#" class="kui-resources-link">
             <span>Usage</span>
-            <img src={pagelinkIcon} alt="Link" />
+            <img src={'ui/pagelink.svg'} alt="Link" />
           </a>
           <a href="#" class="kui-resources-link">
             <span>Examples</span>
-            <img src={pagelinkIcon} alt="Link" />
+            <img src={'ui/pagelink.svg'} alt="Link" />
           </a>
         </ul>
         <div class="ibm-grid-col-lg-16-1"></div>
@@ -116,13 +107,13 @@ const IndexPage = () => (
           <div class="ibm-text-tabs">
             <ul role="tablist" class="ibm-tabs" aria-label="Tab navigation">
               <li class="ibm-tab" role="presentation">
-                <a aria-selected="true" role="tab" href={video2} class="ibm-active" data-subtitle="Subtitle for video 1">Text tab num 1</a>
+                <a aria-selected="true" role="tab" href={'videos/video2.mp4'} class="ibm-active" data-subtitle="Subtitle for video 1">Text tab num 1</a>
               </li>
               <li class="ibm-tab" role="presentation">
-                <a role="tab" href={video3} data-subtitle="Subtitle for video 2">Text tab num 2</a>
+                <a role="tab" href={'videos/video3.mp4'} data-subtitle="Subtitle for video 2">Text tab num 2</a>
               </li>
               <li class="ibm-tab" role="presentation">
-                <a role="tab" href={video4} data-subtitle="Subtitle for video 3">Text tab num 3</a>
+                <a role="tab" href={'videos/video4.mp4'} data-subtitle="Subtitle for video 3">Text tab num 3</a>
               </li>
             </ul>
           </div>
@@ -132,7 +123,7 @@ const IndexPage = () => (
         <div class="ibm-grid-col-lg-16-14">
           <div class="kui-video-player">
             <video class="kui-tutorial-video active" muted controls>
-            <source src={video2} type="video/mp4" />
+            <source src={'videos/video2.mp4'} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
