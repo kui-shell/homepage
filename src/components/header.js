13f21d6f6de512fb3aa24ed30caa9295125b8742
Abdón Rodríguez Davila <a@abdonrd.com>
2019-06-11 11:08:10 +0200

    Add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,24 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link to="/">{siteTitle}</Link>
-        <Link to="/page-3">Download</Link>
-        <Link to="/page-3">GitHub</Link>
-        <Link to="/docs">Documentation</Link>
-      </h1>
+const Header = () => (
+  <header class="kui-main-nav">
+    <div class="ibm-grid-container">
+      <div class="ibm-grid-col-lg-16-16 kui-nav-border"></div>
+      <div class="ibm-grid-col-lg-16-1 ibm-grid-col-md-8-1 ibm-grid-col-sm-4-1">
+        <h1>Kui</h1>
+      </div>
+      <div class="ibm-grid-col-lg-16-9 ibm-grid-col-md-8-2 ibm-grid-col-sm-4-0"></div>
+      <div class="ibm-grid-col-lg-16-6 ibm-grid-col-md-8-5 ibm-grid-col-sm-4-3">
+        <button class="icon-button mobile-menu"><img src="./ui/menu.svg"/></button>
+        <nav>
+          <ul class="kui-link-list">
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">Documentation</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: `Kui`,
-}
 
 export default Header
