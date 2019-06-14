3f20360e4747bb08e77c82fbd68c2c01eda59b7d
Yeray Darias <ydarias@gmail.com>
2019-06-14 11:06:08 +0200

    Fixed Countly reference

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const serverUrl = process.env.KUI_COUNTLY_SERVER || 'https://analytics.kui.us-south.containers.appdomain.cloud';
+const serverUrl = process.env.KUI_COUNTLY_SERVER || 'https://analytics.thesoftpotatos.com';
 
-const appKey = process.env.KUI_COUNTLY_KEY || '3cb27b4529db2bf59eeeecd05a317135a640f1b4';
+const appKey = process.env.KUI_COUNTLY_KEY || 'ebba657ab9271652f02eb343e83a54063f3de841';
 
 const pixelUrl = `${serverUrl}/pixel.png?app_key=${appKey}&begin_session=1`;
 
