fb3de05220656d1b0d4b773225afe69358765360
Abdón Rodríguez Davila <a@abdonrd.com>
2019-06-20 13:43:43 +0200

    Add grey border to the main video

--- src/components/kui-video.js
+++ src/components/kui-video.js
@@ -11,7 +11,7 @@ return (
             </video>
         </MediaQuery>
         <MediaQuery query="(orientation: landscape)">
-            <video muted controls className="kui-hero-video" poster="videos/KUI_Horizontal_poster.jpg">
+            <video muted controls className="kui-hero-video horizontal" poster="videos/KUI_Horizontal_poster.jpg">
             <source src="videos/KUI_Horizontal.mp4" type="video/mp4" />
             Your browser does not support the video tag.
             </video>
