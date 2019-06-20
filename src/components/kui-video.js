9965d64962d315eaec5df69bb8b1634a8d861e26
Yeray Darias <ydarias@gmail.com>
2019-06-20 11:31:43 +0200

    Removed play icon from video posters

--- src/components/kui-video.js
+++ src/components/kui-video.js
@@ -5,13 +5,13 @@ export const KuiVideo = () => {
 return (
     <div className="ibm-grid-col-lg-16-14">
         <MediaQuery query="(orientation: portrait)">
-            <video muted controls className="kui-hero-video" poster="images/title-video-portrait.jpg">
+            <video muted controls className="kui-hero-video" poster="videos/KUI_Vertical_poster.jpg">
             <source src="videos/KUI_Vertical.mp4" type="video/mp4" />
             Your browser does not support the video tag.
             </video>
         </MediaQuery>
         <MediaQuery query="(orientation: landscape)">
-            <video muted controls className="kui-hero-video" poster="images/title-video-landscape.jpg">
+            <video muted controls className="kui-hero-video" poster="videos/KUI_Horizontal_poster.jpg">
             <source src="videos/KUI_Horizontal.mp4" type="video/mp4" />
             Your browser does not support the video tag.
             </video>
